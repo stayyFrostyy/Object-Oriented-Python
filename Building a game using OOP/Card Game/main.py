9fd93413211bf403b658823ab2bd64e4f7dda987
frosty <ashh.gould@gmail.com>
2022-10-13 11:31:36 +0100

    Update main.py

--- Building a game using OOP/Card Game/main.py
+++ Building a game using OOP/Card Game/main.py
@@ -10,6 +10,28 @@ while len(deck) > 0:
     for hand in hands:
         hand.add_card(deck.pop_card())
 
+print(hands[0])
+
+for i in range(13):
+    input()
+    played_cards = []
+    for hand in hands:
+        played_cards.append(hand.pop_card())
+    
+    winner_card = max(played_cards)
+
+    winner_hand = hands[played_cards.index(winner_card)]
+    winner_hand.round_winner()
+
+    print(f"R{i}: " + ' '.join([str(card) for card in played_cards]) + f' Winner: {winner_hand.get_label()} {str(winner_card)}')
 
 for hand in hands:
-    print(hand)
+    scores = []
+    scores.append(hand.get_win_count())
+    print(f"Score for {hand.get_label()}: {hand.get_win_count()}")
+
+print(f"WINNER IS ..... {hands[scores.index(max(scores))].get_label()}")
+
+
+# for hand in hands:
+#     print(hand)
